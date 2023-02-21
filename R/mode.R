68dbbfd9401264c53a1cfc60e0b720315672d137
Lukas Jung <jung-lukas@gmx.net>
2023-02-21 03:26:36 +0100

    comment cleanup

--- R/mode.R
+++ R/mode.R
@@ -45,17 +45,13 @@ mode_first <- function(x, na.rm = FALSE, first_known = TRUE) {
     return(mode1)
   }
   # What if some values really are missing?
-  # Unlike the other mode functions, `mode_first()`
-  # needs to check whether the first-appearing value
-  # (or, by default of `first_known = TRUE`, the
-  # first-appearing value that is known to be a mode)
-  # is as frequent or more as all other values taken
-  # together.
-  # The next few steps determine the maximum number
-  # of possible instances of the second-most
-  # frequent value. The goal is to test whether
-  # this value might contest `mode1`' s status
-  # as the first-appearing mode:
+  # The next few steps determine the number of
+  # instances of `mode1` and the maximum number of
+  # possible instances of the second-most frequent
+  # value (i.e., with the count of all `NA`s added).
+  # The goal is to test whether the latter might
+  # contest `mode1`'s status as the first-appearing
+  # mode:
   count_mode1 <- max(tab)
   count_mode2_na <- sort(tab, decreasing = TRUE)[-1L]
   if (!length(count_mode2_na)) {
@@ -64,36 +60,15 @@ mode_first <- function(x, na.rm = FALSE, first_known = TRUE) {
   count_mode2_na <- max(count_mode2_na) + length(x[is.na(x)])
   # The highest count -- that of `mode1` --
   # may decide the outcome right below.
-  # If it's higher than the highest possible
+  # If it's lower than the highest possible
   # count of any other value (`count_mode2_na`),
-  # it follows that `mode1` is the mode.
+  # it's not known to be the mode.
+  # But if it's higher than that value, or if
+  # `first_known = TRUE` (the default), it is:
   if (count_mode1 < count_mode2_na) {
     return(methods::as(NA, typeof(x)))
   } else if (first_known || count_mode1 > count_mode2_na) {
     return(mode1)
-  #   # Otherwise, the only way the mode might
-  #   # still be determined is for the highest
-  #   # count to be at least half of the full
-  #   # length of the input:
-  # } else if (count_mode1 < length(x) / 2) {
-  #   return(methods::as(NA, typeof(x)))
-    # # `mode_first()` is agnostic about other modes.
-    # # By default (`first_known = TRUE`), it returns
-    # # either the first-appearing value that is known
-    # # to be a mode or `NA`. That's a pragmatic default.
-    # # If a value might be a mode depending on true values
-    # # behind `NA`s, and it appears before the first `mode1`
-    # # value, the function still returns `mode1`.
-    # # Set `first_known` to `FALSE` to make the function
-    # # return `NA` in such cases. The idea is that, strictly
-    # # speaking, the true first mode is unknown here.
-    # # For example, `mode_first(c(2, 1, 1, NA))` returns `1`
-    # # although `2` appears first -- and `2` might be another
-    # # mode, but the first value that is known to be a mode
-    # # is `1`. `mode_first(c(2, 1, 1, NA), first_known = FALSE)`
-    # # returns `NA`.
-  # } else if (first_known) {
-  #   return(mode1)
   }
   # Check whether there is only a single unique known
   # value (i.e., `mode1` ). If so, and if it's the
@@ -203,8 +178,7 @@ mode_single <- function(x, na.rm = FALSE) {
   # values were removed already.
   mode1 <- mode_all(x, na.rm)
   # As the name says, if the distribution
-  # has a single mode (that passes the
-  # `NA` test), that value is returned:
+  # has a single mode, return that value:
   if (length(mode1) == 1L) {
     mode1
     # Multimodal distributions are always `NA`.
@@ -233,10 +207,13 @@ mode_single <- function(x, na.rm = FALSE) {
 #' @examples
 mode_count <- function(x, na.rm = FALSE) {
   modes <- mode_all(x, na.rm)
-  # If the set if modes can't be determined, the number of modes is an unknown
-  # integer. The length of `modes` is tested beforehand because it quickly rules
-  # out cases where `NA` testing is not necessary, and because multiple return
-  # values from `is.na()` would lead to an error in `if`:
+  # If the set if modes can't be determined,
+  # the number of modes is an unknown integer.
+  # The length of `modes` is tested beforehand
+  # because it quickly rules out cases where
+  # `NA` testing is not necessary, and because
+  # multiple return values from `is.na()` would
+  # lead to an error in `if`:
   if (length(modes) == 1L && is.na(modes)) {
     NA_integer_
   } else {
@@ -245,7 +222,7 @@ mode_count <- function(x, na.rm = FALSE) {
 }
 
 
-# Helper function; not exported but called within all exported functions:
+# Helper function; not exported but called within `mode_all()`:
 decide_mode_na <- function(x, ux, mode1) {
   if (length(ux) == 1L) {
     if (length(x[is.na(x)]) < length(x) / 2) {
@@ -264,59 +241,3 @@ decide_mode_na <- function(x, ux, mode1) {
   }
 }
 
-
-# decide_mode_na <- function(x, ux, mode1, allow_tie) {
-#   # Some values might be missing. We need to check
-#   # whether there are so many missings that the most
-#   # frequent known value, `mode1`, might be less
-#   # frequent than the second-most frequent one (or
-#   # the first value tied with `mode1` but appearing
-#   # after it) if all the `NA`s mask the latter.
-#   # To do so, we need to find this second value.
-#   # We look for a possible mode much like above,
-#   # but this time, we exclude the `mode1` values:
-#   mode2 <- ux[which.max(tabulate(match(x[x != mode1], ux)))]
-#   # Count instances of the three relevant available
-#   # values -- most and second-most frequent known
-#   # values plus missing values:
-#   count_mode1 <- length(x[x == mode1])
-#   count_mode2_na <- length(x[x == mode2]) + length(x[is.na(x)])
-#   # `mode_first()` only require `mode1` to be
-#   # at least as frequent as the sum of the
-#   # other two counts. That is because it is
-#   # only interested in getting the first mode,
-#   # whether or not there are other modes
-#   # beyond that. However, `mode_all()` and
-#   # `mode_single()` are more strict because
-#   # they are meant to rule out that the true
-#   # count of `mode2` is just as high as that
-#   # of `mode1`. If this is even possible,
-#   # these functions need to return `NA` --
-#   # `mode_all()` because it needs to be sure
-#   # about the set of modes, and `mode_single()`
-#   # because it only allows for one mode.
-#   if (allow_tie) {
-#     mode1_frequent_enough <- count_mode1 >= count_mode2_na
-#   } else {
-#     mode1_frequent_enough <- count_mode1 > count_mode2_na
-#   }
-#   # (Assuming `allow_tie = TRUE`:)
-#   # `mode1` is the true mode only if it's
-#   # at least as frequent as the sum of the
-#   # other two counts. Otherwise, if all the
-#   # `NA`s mask `mode2` values, the true count
-#   # of `mode2` would be higher than that of
-#   # `mode1`. We don't know which values hide
-#   # behind `NA`, so we can't rule out this
-#   # second scenario if the known count of
-#   # `mode1` is lower than a possibly true
-#   # count of `mode2`. Therefore, if
-#   # `count_mode1` is not large enough, the
-#   # function returns `NA` (coerced to the
-#   # same type as the input, `x`):
-#   if (mode1_frequent_enough) {
-#     mode1
-#   } else {
-#     methods::as(NA, typeof(x))
-#   }
-# }
