1c54a8dedb84b67712e62bdb26f522daa7f67e92
Lukas Jung <jung-lukas@gmx.net>
2024-01-10 01:48:09 +0100

    fix bug in `mode_is_trivial()`
    * It used to ignore `max_unique` if all known values were equally frequent.
    * Also, some light refactoring and better docs.

--- R/predicates.R
+++ R/predicates.R
@@ -1,33 +1,37 @@
 #' Is the mode trivial?
 #'
 #' @description `mode_is_trivial()` checks whether all values in a given vector
-#'   are equally frequent. The mode is not too informative in such cases.
+#'   are equally frequent, i.e., uniformly distributed. The mode is not too
+#'   informative in such cases.
 #'
 #' @inheritParams mode_all
 #' @param max_unique Numeric or string. If the maximum number of unique values
-#'   in `x` is known, set `max_unique` to that number. This rules out that `NA`s
-#'   represent values beyond that number (see examples). Set it to `"known"`
-#'   instead if no values beyond those already known can occur. Default is
+#'   in `x` is known, set `max_unique` to that number. This puts a limit on how
+#'   many unique values may be represented by `NA`s. Set it to `"known"` instead
+#'   if no values beyond those already known can occur in `x`. Default is
 #'   `NULL`, which assumes no maximum.
 #'
-#' @details The function returns `TRUE` whenever `x` has length < 3 because no
-#'   value is more frequent than another one. Otherwise, it returns `NA` in
-#'   these cases:
-#'   - Some `x` values are missing, all known values are equal, and the number
-#'   of missing values is divisible by the number of unique known values. Thus,
-#'   the missings don't necessarily break the tie among known values, and it is
-#'   unknown whether there is a value with a different frequency.
-#'   - All known values are modes if the `NA`s "fill up" the non-modal values
-#'   exactly, i.e., without any `NA`s remaining.
-#'   - Some `NA`s remain after "filling up" the non-modal values with `NA`s (so
-#'   that they are hypothetically modes), and the number of remaining `NA`s is
-#'   divisible by the number of unique known values.
+#' @details The function returns `TRUE` if `x` has length < 3 because no value
+#'   is more frequent than another one. Otherwise, it returns `NA` in these
+#'   cases:
+#'   - Some `x` values are missing, all known values are equal, and `max_unique`
+#'   is not `1` or `"known"`. Thus, it is unknown whether there are values with
+#'   different frequencies.
+#'   - All known values are modes if the `NA`s "fill up" all non-modal known
+#'   values exactly, i.e., without any `NA`s remaining. These values might then
+#'   hypothetically be modes.
 #'   - There are so many missing values that they might form mode-sized groups
 #'   of values that are not among the known values, and the number of `NA`s is
 #'   divisible by the modal frequency so that all (partly hypothetical) values
 #'   might be equally frequent. You can limit the number of such hypothetical
 #'   values by specifying `max_unique`. The function might then return `FALSE`
 #'   instead of `NA`.
+#'   - Some `NA`s remain after "filling up" the non-modal known values with
+#'   `NA`s and / or hypothetically forming new mode-sized groups, and the number
+#'   of remaining `NA`s is divisible by the number of unique known values. Thus,
+#'   these surplus `NA`s can be evenly distributed across the known (or,
+#'   perhaps, partly hypothesized) values. Thus, a uniform distribution is still
+#'   possible. `max_unique` can be relevant here, as well.
 #'
 #' @return Logical (length 1).
 #'
@@ -38,11 +42,11 @@
 #' # all values are equal...
 #' mode_is_trivial(c(1, 1, 1))
 #'
-#' # ...and even if all unique
-#' # values are equally frequent:
+#' # ...and even if there are multiple unique
+#' # values but they are all equally frequent:
 #' mode_is_trivial(c(1, 1, 2, 2))
 #'
-#' # It's also trivial if
+#' # This includes cases where
 #' # all values are different:
 #' mode_is_trivial(c(1, 2, 3))
 #'
@@ -63,53 +67,71 @@
 #' mode_is_trivial(c(7, 7, 7, 8, rep(NA, 5)), max_unique = "known")
 
 mode_is_trivial <- function(x, na.rm = FALSE, max_unique = NULL) {
-  if (na.rm) {
-    x <- x[!is.na(x)]
-  }
-  # There are some edge cases to check for. With less than three values, none
-  # can be more frequent than the other:
-  if (length(x) < 3L) {
-    return(TRUE)
-  }
   n_x <- length(x)
   x <- x[!is.na(x)]
-  n_na <- n_x - length(x)
-  rm(n_x)
+  if (na.rm) {
+    n_x <- length(x)
+    n_na <- 0L
+  } else {
+    n_na <- n_x - length(x)
+  }
   unique_x <- unique(x)
   max_unique <- handle_max_unique_input(
     x, max_unique, length(unique_x), n_na, "mode_is_trivial"
   )
-
-  # If some values are missing (from a length >= 3 vector) and all known values
-  # are equal, it's unknown whether there is a value with a different frequency:
-  if (n_na > 0L && length(unique_x) == 1L) {
-    return(NA)
+  # Some edge cases to check for:
+  # -- With less than three values, none can be more frequent than another.
+  # -- In the next most simple case, all values are known, and it is trivial to
+  # determine whether all of them are equally frequent.
+  if (n_x < 3L) {
+    return(TRUE)
+  } else if (n_na == 0L) {
+    frequency_known <- table(x)
+    uniformity_is_true <- all(frequency_known == frequency_known[1L])
+    return(uniformity_is_true)
   }
-  modes <- mode_all_if_no_na(x)
-  # Special rules apply if all known values are equally frequent:
-  # -- If there are no `NA`s, there are no values beyond the known ones, so
-  # `TRUE` is returned.
-  # -- If the number of `NA`s can be divided by the number of unique values
-  # (here, the number of modes), sets of `NA`s may remain, each of the size of
-  # the modal frequency, and form entirely different values, i.e., different
-  # from the known values in `x`. This latter scenario is far from certain, but
-  # it cannot be ruled out (unless the user does so by setting `max_unique` to
-  # `"known"`, which doesn't matter here), so `NA` is returned. This is also
-  # true in one case that is not captured by the modulo: Both the number of
-  # missing values and the modal frequency are exactly 1.
-  # -- If this is not case but there are still `NA`s, at least some of these
-  # must belong to less frequent values, so `FALSE` is returned.
-  if (all(unique_x %in% modes)) {
-    if (n_na == 0L) {
+  # Special rules apply in case all known values are equal:
+  # -- If the user specified `max_unique` such that no unique values beyond the
+  # single known one can exist, all values including the `NA`s must be equal.
+  # -- Otherwise, there may or may not be other values with different
+  # frequencies than the known one. Due to this uncertainty, `NA` is returned.
+  if (length(unique_x) == 1L) {
+    if (!is.null(max_unique) && max_unique == 1) {
       return(TRUE)
-    } else if (
-      n_na %% length(modes) == 0L ||
-      (n_na == 1L && length(x) == length(unique_x))
-    ) {
-      return(NA)
     } else {
-      return(FALSE)
+      return(NA)
     }
+  }
+  # Determine the set of modes among known values using an internal helper:
+  modes <- mode_all_if_no_na(x)
+  # Special rules also apply if all known values are equally frequent:
+  # -- If both the number of missing values and the modal frequency are exactly
+  # one, the one missing value may or may not represent one of the known values,
+  # barring `max_unique` specifications that rule this out. This scenario can't
+  # be affirmed or ruled out, so `NA` is returned.
+  # -- Uniformity is also unknown if the number of `NA`s can be divided by the
+  # number of unique values (which is the number of modes here). The `NA`s might
+  # then be evenly distributed across the unique values. Equivalently, sets of
+  # `NA`s may remain, each of the size of the modal frequency, and form entirely
+  # different values, i.e., different from the known values in `x`. This, too,
+  # might be ruled out by `max_unique`.
+  # -- If neither of this is the case, the `NA`s must belong to values that are
+  # more or less frequent than others, so `FALSE` is returned.
+  if (all(unique_x %in% modes)) {
+    uniformity_is_unknown_single_na <- n_na == 1L &&
+      length(x) == length(unique_x) &&
+      !isTRUE(max_unique == length(unique_x))
+    if (uniformity_is_unknown_single_na) {
+      return(NA)
+    }
+    uniformity_is_unknown_modulo <- n_na %% length(modes) == 0L || (
+      !isTRUE(max_unique == length(unique_x)) &&
+        n_na %% length(x[x == modes[[1L]]]) == 0L
+    )
+    if (uniformity_is_unknown_modulo) {
+      return(NA)
+    }
+    return(FALSE)
   }
   # Reduce `n_na` to the number of `NA`s that are left after distributing other
   # `NA`s among the non-modal values such that they become modes in a
@@ -119,7 +141,7 @@ mode_is_trivial <- function(x, na.rm = FALSE, max_unique = NULL) {
   frequency_max <- length(x[x %in% modes[[1L]]])
   # Some more special rules, this time concerning user-imposed restrictions on
   # the number of unique values in `x`:
-  if (!is.null(max_unique) && n_na > 0L) {
+  if (!is.null(max_unique)) {
     n_slots_known_vals <- frequency_max * length(unique_x)
     n_slots_new_vals <- frequency_max * (max_unique - length(unique_x))
     if (n_slots_new_vals == 0L) {
@@ -131,34 +153,28 @@ mode_is_trivial <- function(x, na.rm = FALSE, max_unique = NULL) {
     }
     n_slots_all <- n_slots_known_vals + n_slots_new_vals
     n_na_super_surplus <- n_na_surplus - n_slots_new_vals
-    if (
-      n_na_super_surplus == 0L ||
+    uniformity_is_unknown <- n_na_super_surplus == 0L ||
       (n_na_super_surplus > 1L && n_slots_all %% n_na_super_surplus == 0L) ||
       (n_na_surplus > 1L && n_slots_known_vals %% n_na_surplus == 0L)
-    ) {
+    if (uniformity_is_unknown) {
       return(NA)
     } else {
       return(FALSE)
     }
   }
-  # -- If the "count" of missing values is negative after attempting to fill up
-  # the empty slots, it means the empty slots cannot be filled with `NA`s, so
-  # the `x` values must differ in their true frequencies.
-  # -- If their count (zero or positive) can be divided by the number of unique
-  # values, these `NA`s might fill up the empty slots, so there is a possibility
-  # that all values are equally frequent. The same is true if the count of
-  # remaining `NA`s can be divided by the modal frequency because they might
-  # represent values beyond the known ones; see above.
+  # -- If the number of missing values is zero or positive and it can be divided
+  # by the number of unique values, these `NA`s might fill up the empty slots,
+  # so there is a possibility that all values are equally frequent. The same is
+  # true if the count of remaining `NA`s can be divided by the modal frequency
+  # because they might represent values beyond the known ones; see above.
   # -- Otherwise, there are some `NA`s that cannot be part of a group with the
   # modal frequency. They must be values with a lesser frequency, so there are
-  # different frequencies among true `x` values.
-  if (
-    n_na_surplus >= 0L &&
+  # different frequencies among `x` values.
+  uniformity_is_unknown <- n_na_surplus >= 0L &&
     any(n_na_surplus %% c(length(unique_x), frequency_max) == 0L)
-  ) {
+  if (uniformity_is_unknown) {
     NA
   } else {
     FALSE
   }
 }
-
