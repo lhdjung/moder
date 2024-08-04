8afddcba033ed6087c1125826db60c8c176a6c01
Lukas Jung <jung-lukas@gmx.net>
2024-08-04 17:02:27 +0200

    rework predicate function docs

--- tests/testthat/test-predicates.R
+++ tests/testthat/test-predicates.R
@@ -1,4 +1,34 @@
 
+test_that("`is_unimodal()` works correctly", {
+  expect_true(is_unimodal(x1))
+  expect_equal(is_unimodal(x2), NA)
+  expect_true(is_unimodal(x3))
+  expect_false(is_unimodal(x4))
+  expect_equal(is_unimodal(x5), NA)
+  expect_true(is_unimodal(x6))
+  expect_false(is_unimodal(x7))
+  expect_equal(is_unimodal(x8), NA)
+  expect_equal(is_unimodal(x9), NA)
+  expect_true(is_unimodal(x10))
+  expect_equal(is_unimodal(x11), NA)
+  expect_equal(is_unimodal(x12), NA)
+  expect_equal(is_unimodal(x13), NA)
+  expect_equal(is_unimodal(x14), NA)
+  expect_equal(is_unimodal(x15), NA)
+  expect_equal(is_unimodal(x16), NA)
+  expect_equal(is_unimodal(x17), NA)
+})
+
+# Don't need to test against all of the same vectors again:
+test_that("`is_multimodal()` works correctly", {
+  expect_false(is_multimodal(x1))
+  expect_equal(is_multimodal(x2), NA)
+  expect_false(is_multimodal(x3))
+  expect_true(is_multimodal(x4))
+  expect_equal(is_multimodal(x5), NA)
+})
+
+
 test_that("`mode_is_trivial()` works correctly", {
   expect_equal(mode_is_trivial(x1 ), FALSE)
   expect_equal(mode_is_trivial(x2 ), NA)
